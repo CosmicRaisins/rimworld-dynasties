dc705755fbc915393a768ce7e396b59fd29ce5cf
CosmicRaisins <60367004+CosmicRaisins@users.noreply.github.com>
2021-02-17 14:20:17 -0500

    Uploaded current progess
    - Faction:
    - added faction icon
    - added faction name generator
    - added faction base name generator
    - added faction royal titles
    
    Animals:
    Baihu:
    - added north and east texure
    - finished def writing
    - added code for thought projection
    - added though def
    Bodan:
    - added all textures
    - made progress on def writing

---
+++ Source/Dynasties/Comps/CompThoughtProjector.cs
@@ -0,0 +1,61 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Verse;
+using Verse.Sound;
+using UnityEngine;
+using System.Collections;
+
+namespace SZ.PawnComps
+{
+    public class CompThoughtProjector : ThingComp
+    {
+        public int tickCounter = 0;
+        public List<Pawn> pawnList = new List<Pawn>();
+        public Pawn thisPawn;
+
+
+        public CompProperties_ThoughtProjector Props
+        {
+            get
+            {
+                return (CompProperties_ThoughtProjector)this.props;
+            }
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            tickCounter++;
+            if (tickCounter > Props.tickInterval)
+            {
+                thisPawn = this.parent as Pawn;
+                if (thisPawn != null && thisPawn.Map != null && !thisPawn.Dead && !thisPawn.Downed)
+                {
+                    foreach (Thing thing in GenRadial.RadialDistinctThingsAround(thisPawn.Position, thisPawn.Map, Props.radius, true))
+                    {
+                        Pawn pawn = thing as Pawn;
+                        if (pawn != null && !pawn.AnimalOrWildMan() && pawn.RaceProps.IsFlesh)
+                        {
+                            if (!pawn.Dead && !pawn.Downed && pawn.GetStatValue(StatDefOf.PsychicSensitivity, true) > 0f)
+                            {
+                                if (Props.showEffect)
+                                {
+                                    Find.TickManager.slower.SignalForceNormalSpeedShort();
+                                    SoundDefOf.PsychicPulseGlobal.PlayOneShot(new TargetInfo(this.parent.Position, this.parent.Map, false));
+                                    MoteMaker.MakeAttachedOverlay(this.parent, ThingDef.Named("Mote_PsycastPsychicEffect"), Vector3.zero, 1f, -1f);
+                                }
+                                if (pawn.needs != null && pawn.needs.mood != null)
+                                {
+                                    pawn.needs.mood.thoughts.memories.TryGainMemory(ThoughtDef.Named(Props.thoughtDef), null);
+                                }
+                            }
+                        }
+                    }
+                }
+                tickCounter = 0;
+            }
+        }
+    }
+}
---
+++ Source/Dynasties/Comps/Properties/CompProperties_ThoughtProjector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+
+namespace SZ.PawnComps
+{
+    public class CompProperties_ThoughtProjector : CompProperties
+    {
+        public int radius = 1;
+        public int tickInterval = 1000;
+        public string thoughtDef = "AteWithoutTable";
+        public bool showEffect = false;
+
+        public CompProperties_ThoughtProjector()
+        {
+            this.compClass = typeof(CompThoughtProjector);
+        }
+
+
+    }
+}
